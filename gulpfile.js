ac18e92f37180e9406bab72ce33f6139ed24311e
michelpm1 <michelpm1@gmail.com>
2016-10-25 21:40:33 +0200

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
+var output = './css/';
+var gulpDocs = require('gulp-ngdocs');
+
+
+// gulp.task('webser', function() {
+//     connect.server({
+//         livereload: true
+//     });
+// });
+
+gulp.task('serveprod', function() {
+    connect.server({
+        port: process.env.PORT || 5000, // localhost:5000
+        livereload: false
+    });
+});
+
+gulp.task('default', ['serveprod']);
